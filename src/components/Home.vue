5927197ed2c95a076f51a4f48e567ab572ac0aff
nakel <xarroyovalera@gmail.com>
2017-07-30 11:42:58 +0200

    Link igpost to movie api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,7 @@
 <template>
   <div>
-    <header v-if="genres.length" class="header">
-      <dropdown
-        filterName="genre"
-        :items="genres"
-        :selected.sync="selectedGenre">
-      </dropdown>
-    </header>
-
     <div v-if="movies.length" class="movies">
-      <movie v-for="movie in filteredMovies" :movie="movie">
-      </movie>
+      <igpost v-for="item in filteredMovies" :title="item.title" :image="item.poster_path"></igpost>
     </div>
   </div>
 </template>
@@ -18,8 +9,7 @@
 <script>
 import { mapState, mapActions } from 'vuex'
 import api from '../services/api'
-import dropdown from './Dropdown'
-import movie from './Movie'
+import igpost from './Item'
 
 export default {
   name: 'hello',
@@ -56,12 +46,9 @@ export default {
     this.getGenres()
   },
   components: {
-    dropdown,
-    movie
+    igpost,
   }
 }
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss" src="../assets/scss/index.scss"></style>
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,6 +1,10 @@
 <template lang="html">
-   <md-card class="item-card" md-with-hover>
-    <md-card-media><img :src="image" ></md-card-media> 
+   <md-card class="item-card">
+
+    <md-card-media>
+      <img :src="`https://image.tmdb.org/t/p/w500/${ image }`" alt="">
+    </md-card-media>
+
     <md-card-header>
       <div class="md-title">{{ title }}</div> 
       <md-chip v-for="hashtag in hashtags">{{ hashtag }}</md-chip>
@@ -10,6 +14,7 @@
       <md-button>Share</md-button>
       <md-button>Like!</md-button>
     </md-card-actions>
+    
   </md-card> 
 </template>
 
@@ -23,14 +28,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-
-.item-card {
-  // margin: 1em;
-  width: 100%;
-
-  img {
-    max-height: 350px;
-  };
-}
-</style>
+<style lang="scss"></style>
